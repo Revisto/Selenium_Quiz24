6d660fffe8542d63094e6558c0a5717b85f47aff
revisto <walt.shabani@gmail.com>
2020-04-14 14:03:42 +0430

    Update

--- Selenium.py
+++ Selenium.py
@@ -10,7 +10,7 @@ for i in range (3):
 
     Url=input("URL:")
 
-    for i in range (30):
+    for i in range (1):
         Path="/usr/lib/chromium-browser/chromedriver"
         Drive = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
         Drive.get(Url)
@@ -20,7 +20,7 @@ for i in range (3):
 
         SearchBox = Drive.find_elements_by_class_name("form-control")
 
-        SearchBox[0].send_keys("کلاس شما به دلیل فعالیت های مشکوک بسته شده است")
+        SearchBox[0].send_keys("کلاس شما به دلیل فعالیت های مشکوک بسته شده است"+str(i))
         SearchBox[0].send_keys(Keys.ENTER)
 
         SearchBox[1].send_keys("0912"+str(random.randint(1000000,9999999)))
@@ -31,3 +31,6 @@ for i in range (3):
         SearchBox[2].send_keys("1234567")
         SearchBox[2].send_keys(Keys.ENTER)
 
+        time.sleep(5)
+        Drive.find_element_by_class_xpath("/html/body/div[2]/div/div/div[1]/div/div/span/button[2]/span[1]").click()
+        
